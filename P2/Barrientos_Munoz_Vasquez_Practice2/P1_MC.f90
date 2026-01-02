8fb08729161e4e4743f2e26a9a17f24efa9d1cbf
Andrea Muñoz Rubio <anmuru2003@gmail.com>
2026-01-02 21:20:45 +0100

    Update P1_MC.f90

--- P2/Barrientos_Munoz_Vasquez_Practice2/P1_MC.f90
+++ P2/Barrientos_Munoz_Vasquez_Practice2/P1_MC.f90
@@ -7,12 +7,16 @@ PROGRAM P1_MC
 
 IMPLICIT NONE
 
-  REAL*16 :: Delta_E, E
-  INTEGER :: M, N, MCS, x, r1279, S_i
-  INTEGER, ALLOCATABLE :: nbr_array(:,:), s_array(:), s_possible_array(:), table(:)
+  REAL*16 :: E
+  INTEGER :: M, N, MCS, x, S_i
+  INTEGER, ALLOCATABLE :: nbr_array(:,:), s_array(:), s_possible_array(:)
+  REAL*16, ALLOCATABLE :: table(:)
+  INTEGER :: Delta_E, idx
+  REAL*4 :: r1279
   N = L**2
-
-  allocate(nbr_array(4, N), s_array(N), s_possible_array(N), table(4))
+        
+  call setr1279(1234)
+  allocate(nbr_array(4, N), s_array(N), s_possible_array(N), table(9))
 
   CALL neighbors(L, nbr_array)
   
@@ -20,20 +24,20 @@ IMPLICIT NONE
   
   CALL write_table(table)
 
-  open (unit=10, file="Energy.dat", status="replace", iostat=ios) 
+  open (unit=10, file="Energy.dat", status="replace") 
   do x = 1, num_MCS
         CALL spin_change(s_array, N, s_possible_array, S_i)
-        Delta_E = 2 * s_possible_array(S_i) * SUM( s_array(nbr_array(:, S_i)))
-        
-        if (Delta_E < 0) then
-                s_array = s_possible_array
-        else
-                if (r1279() < table(int(Delta_E))) then
-                        s_array = s_possible_array
-                end if
-        end if
-        if (mod(x, 1000) == 0) then
-                CALL CALSC_E_M(s_array, nbr_array, L, E, M)
+        Delta_E = 2 * s_possible_array(S_i) * sum(s_array(nbr_array(:, S_i)))
+	if (Delta_E < 0) then
+    		s_array(S_i) = s_possible_array(S_i)
+	else
+    		idx = (Delta_E + 2*4)/2 + 1
+	        if (r1279() < table(idx)) then
+	            s_array(S_i) = s_possible_array(S_i)
+		end if
+	end if
+        if (mod(x, 10000) == 0) then
+                CALL CALC_E_M(s_array, nbr_array, L, E, M)
                 write(10, *) E
                 print *, x
           end if
